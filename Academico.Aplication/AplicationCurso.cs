d7c7e7402dc2b34798161c905fa8a75c03644fab
Celielton Ramalho <Celielton Ramalho>
2016-12-15 22:11:52 -0200

    Fim Aplication Service

---
+++ Academico.Aplication/AplicationCurso.cs
@@ -0,0 +1,15 @@
+﻿using Academico.Aplication.Interfaces;
+using Academico.Dominio.Entidades;
+using Academico.Dominio.Interfaces.Repositorios;
+
+namespace Academico.Aplication
+{
+    public class AplicationCurso : AplicationBase<Curso>, IAplicationCurso
+    {
+        private readonly ICursoRepository _cursoRepository;
+        public AplicationCurso(ICursoRepository cursoRespository) : base(cursoRespository)
+        {
+            _cursoRepository = cursoRespository;
+        }
+    }
+}
---
+++ Academico.Aplication/DisciplinaAplication.cs
@@ -0,0 +1,15 @@
+﻿using Academico.Aplication.Interfaces;
+using Academico.Dominio.Entidades;
+using Academico.Dominio.Interfaces.Repositorios;
+
+namespace Academico.Aplication
+{
+    public class DisciplinaAplication : AplicationBase<Disciplina>, IAplicationDisciplina
+    {
+        private readonly IDisciplinaRepository _disciplinaRepository;
+        public DisciplinaAplication(IDisciplinaRepository disciplinaRepository) : base(disciplinaRepository)
+        {
+            _disciplinaRepository = disciplinaRepository;
+        }
+    }
+}
